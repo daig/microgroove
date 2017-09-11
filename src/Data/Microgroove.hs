3892b4fc219f082d4c73bd6ec73ff2b78f4ccc23
Dai <dailectic@gmail.com>
2017-09-10 23:39:51 -0400

    Added checkIndex

--- src/Data/Microgroove.hs
+++ src/Data/Microgroove.hs
@@ -1,6 +1,8 @@
 {-# language MagicHash #-}
 {-# language MultiParamTypeClasses #-}
+{-# language FlexibleContexts #-}
 {-# language UndecidableSuperClasses #-}
+{-# language UndecidableInstances #-}
 {-# language AllowAmbiguousTypes #-}
 module Data.Microgroove where
 import Data.Vector (Vector)
@@ -12,6 +14,9 @@ import GHC.Exts (Any,Constraint)
 import Control.Monad.ST
 import Control.Monad.Primitive (PrimMonad(..))
 
+import GHC.TypeLits
+import Data.Proxy
+
 cast# :: forall b a. a -> b
 cast# = unsafeCoerce
 
@@ -19,6 +24,13 @@ newtype Rec (f :: u -> *) (us :: [u]) = Rec# (V.Vector Any)
 data Rec' (f :: u -> *) (us :: [u]) where
   RNil' :: Rec' f '[]
   RCons' :: f u -> Rec f us -> Rec' f (u ': us)
+
+splitCons :: Rec f (x ': xs) -> (f x,Rec f xs)
+splitCons (Rec# v) = (cast# $ V.head v, Rec# $ V.tail v)
+
+pattern RCons# :: f x -> Rec f xs -> Rec f (x ': xs)
+pattern RCons# x xs <- (( \ (Rec# v) -> (cast# $ V.head v, Rec# $ V.tail v)) -> (x,xs)) where
+  RCons# x (Rec# xs) = Rec# (V.cons (cast# x) xs)
 
 upRec :: Rec f us -> Rec' f us
 upRec (Rec# v) | V.null v = cast# RNil'
@@ -35,24 +47,43 @@ pattern RCons :: () => (us' ~ (u ': us)) => f u -> Rec f us -> Rec f us'
 pattern RCons x xs <- (upRec -> RCons' x xs) where
   RCons x (Rec# xs) = Rec# (V.cons (cast# x) xs)
 
-newtype RIndex x xs = RIndex# Int deriving Show
-data RIndex' x xs where
-  RZ' :: RIndex' x (x ': xs)
-  RS' :: RIndex x xs -> RIndex' x (y ': xs)
-upRIndex :: RIndex x xs -> RIndex' x xs
+
+type family ((xs :: [u]) !! (n :: Nat)) where
+  (x ': _) !! 0 = x
+  (_ ': xs) !! n = xs !! (n-1)
+
+newtype RIndex (xs :: [u]) (x :: u) = RIndex# Int deriving Show
+data RIndex' xs x where
+  RZ' :: RIndex' (x ': xs) x
+  RS' :: RIndex xs x -> RIndex' (y ': xs) x
+upRIndex :: RIndex xs x -> RIndex' xs x
 upRIndex (RIndex# i) = case i of
   0 -> cast# RZ'
   _ -> cast# $ RS' $ RIndex# $ i-1
-pattern RZ :: RIndex (x :: u) (x ': xs)
+pattern RZ :: RIndex (x ': xs) (x :: u)
 pattern RZ = RIndex# 0
-pattern RS :: RIndex (x :: u) (xs :: [u]) -> RIndex x (y ': xs)
+pattern RS :: RIndex (xs :: [u]) (x :: u) -> RIndex (y ': xs) x
 pattern RS i <- (upRIndex -> RS' i) where
   RS (RIndex# i) = RIndex# (1+i)
-{-checkIndex :: Rec f (xs :: [u]) -> Int -> Maybe (RIndex (x :: u) (xs :: [u]))-}
-{-checkIndex (RCons (_ :: f u) _)  0 = Just (RZ :: RIndex u xs)-}
-{-checkIndex n = if n > 0 then RS <$> checkIndex (n-1) else Nothing-}
 
-(!) :: Rec f us -> RIndex u us -> f u
+type family Length (xs :: [u]) :: Nat where
+  Length '[] = 0
+  Length (_ ': xs) = 1 + Length xs
+  
+mkIndex :: forall n xs . (KnownNat n, n <= Length xs - 1) => RIndex xs (xs !! n)
+mkIndex = RIndex# $ fromInteger $ natVal (Proxy @n) 
+
+data Some f = forall x. Some (f x)
+data MaybeSome f = forall x. JustSome (f x) | None
+
+index :: forall n f xs. KnownNat n => Rec f xs -> f (xs !! n)
+index (Rec# v) = cast# $ v V.! fromInteger (natVal (Proxy @n))
+checkIndex :: forall (xs :: [u]) f. KnownNat (Length xs) => Rec f xs -> Int -> MaybeSome (RIndex xs)
+checkIndex (Rec# (length -> n)) i | i < n = case someNatVal (fromIntegral i) of
+  Just (SomeNat (Proxy :: Proxy n)) -> JustSome $ RIndex# @u @xs @(xs !! n) i
+  
+
+(!) :: Rec f us -> RIndex us u -> f u
 Rec# v ! RIndex# i = cast# $ v V.! i
 
 aa = RCons (Id (1::Integer)) $ RCons (Id (0.1::Double)) $ RCons (Id (3::Int)) RNil
@@ -66,6 +97,10 @@ type family AllF (c :: * -> Constraint) (f :: u -> *) (xs :: [u]) :: Constraint 
 type family All (c :: u -> Constraint) (xs :: [u]) :: Constraint where
   All c '[] = ()
   All c (x ': xs) = (c x,All c xs)
+
+instance Show (Rec f '[]) where show RNil = "[]"
+instance (Show (f x), Show (Rec f xs)) => Show (Rec f (x ': xs)) where
+  show (RCons a xs) = show a ++ " : " ++ show xs
 
 {-rmap :: (forall x. f x -> g x) -> Rec f xs -> Rec g xs-}
 {-rmap f (Rec# v) = -}

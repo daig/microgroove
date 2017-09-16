4e156670d3da539e4ce40b236930e4a4b058e71f
Dai <dailectic@gmail.com>
2017-09-15 20:08:06 -0700

    Add documentation for MRec

--- src/Data/Microgroove/MRec.hs
+++ src/Data/Microgroove/MRec.hs
@@ -20,28 +20,36 @@ import GHC.TypeLits
 import Data.Proxy
 
 
+-- | A mutable heterogeneous record represented by an untyped mutable vector
 newtype MRec s (f :: u -> *) (us :: [u]) = MRec# (MVector s Any)
+-- | An intermediate type to deconstruct an @MRec@ into head normal form
 data MRec' s (f :: u -> *) (us :: [u]) where
   MRNil' :: MRec' s f '[]
   MRCons' :: MVector s (f u) -> MRec s f us -> MRec' s f (u ': us)
 
+-- | Split a mutable record into a head vector of length one, and the remaining record
+-- must be statically known to be nonempty
 splitCons# :: MRec s f (x ': xs) -> (MVector s (f x),MRec s f xs)
 splitCons# (MRec# v) = (cast# $ VM.take 1 v, MRec# $ VM.tail v)
 
-pattern MRCons# :: MVector s (f x) -> MRec s f xs -> MRec s f (x ': xs)
-pattern MRCons# x xs <- (( \ (MRec# v) -> (cast# $ VM.take 1 v, MRec# $ VM.tail v)) -> (x,xs)) -- where
-
+-- | Convert an MRec to head normal form,
+-- refining the type to distinguish empty from nonempty records
 upMRec :: MRec s f us -> MRec' s f us
 upMRec (MRec# v) | VM.null v = cast# MRNil'
                  | otherwise = cast# $ MRCons' (cast# $ VM.take 1 v) (MRec# $ VM.tail v)
 
+-- | Match an empty record, refining its type
 pattern MRNil :: () => (us ~ '[]) => MRec s f us
 pattern MRNil <- (upMRec -> MRNil')
 
+-- | Match a nonempty record, refining its type
+-- The head is vector of length one to preserve mutable identity
 pattern MRCons :: () => (us' ~ (u ': us)) => VM.MVector s (f u) -> MRec s f us -> MRec s f us'
 pattern MRCons x xs <- (upMRec -> MRCons' x xs)
 
 
+-- | Modify a mutable record in place by mapping a natural tranformation.
+-- O(n)
 rmap :: forall g m f xs. PrimMonad m => (forall x. f x -> g x) -> MRec (PrimState m) f xs -> m (MRec (PrimState m) g xs)
 rmap f xs = cast# xs <$ go xs where
   go :: MRec (PrimState m) f as -> m ()
@@ -49,6 +57,8 @@ rmap f xs = cast# xs <$ go xs where
     x@MRNil -> pure ()
     MRCons x xs -> VM.modify x (castf# @f . f) 0 >> go xs
 
+-- | Modify a mutable record in place by mapping a natural tranformation that can make use of the provided constraint.
+-- Ex: `crmap @Show (K . show) :: (MRec s f xs) -> ST s (MRec s (K String) xs)`
 crmap :: forall (c :: * -> Constraint) g m f xs. (AllF c f xs, PrimMonad m)
      => (forall x. c (f x) => f x -> g x) -> MRec (PrimState m) f xs -> m (MRec (PrimState m) g xs)
 crmap f xs = cast# xs <$ go xs where
@@ -57,6 +67,7 @@ crmap f xs = cast# xs <$ go xs where
     x@MRNil -> pure ()
     MRCons x xs -> VM.modify x (castf# @f . f) 0 >> go xs
 
+-- | Convert a mutable record to a mutable vector by mapping to a homogeneous type
 toMVector :: forall r m f xs. PrimMonad m
          => (forall x. f x -> r) -> MRec (PrimState m) f xs -> m (MVector (PrimState m) r)
 toMVector f xs = cast# xs <$ go xs where
@@ -65,6 +76,7 @@ toMVector f xs = cast# xs <$ go xs where
     x@MRNil -> pure ()
     MRCons x xs -> VM.modify x (cast# . f) 0 >> go xs
 
+-- | Convert a mutable record to a mutable vector by mapping to a homogeneous type, making use of provided constraint
 ctoMVector :: forall (c :: * -> Constraint) r m f xs. (AllF c f xs, PrimMonad m)
           => (forall x. c (f x) => f x -> r) -> MRec (PrimState m) f xs -> m (MVector (PrimState m) r)
 ctoMVector f xs = cast# xs <$ go xs where
